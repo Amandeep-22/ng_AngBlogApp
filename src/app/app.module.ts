98af293195e203dcb9ba2752ea5a5277c89400a1
AmanD Yadav <amand.10712134@ltimindtree.com>
2024-08-14 14:42:02 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { ToastrModule, ToastNoAnimation, ToastNoAnimationModule } from 'ngx-toastr';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment.development';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    FormsModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule
+    //ToastNoAnimationModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/categories.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { ToastrService } from 'ngx-toastr';
+import { map, Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriesService {
+
+  constructor(private afs: AngularFirestore, private toastr: ToastrService) { }
+
+  saveData(Data: any){
+    this.afs.collection('categories').add(Data).then(docRef => {
+      console.log(docRef);
+      this.toastr.success("Data Stored Successfully..!")
+     })
+     .catch(err => { console.log(err)})
+  }
+
+  loadData() {
+    return this.afs.collection('categories').snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return { id, data };
+        })
+  })
+    );
+  }
+
+  updateData(id:any, EditData:any){
+    // this.afs.collection('categories').doc(id).update(EditData).then(docRef => {
+    //   this.toastr.success("Data Updated Successfully..!")
+    // })
+    this.afs.doc(`categories/${id}`).update(EditData).then(docRef => {
+      this.toastr.success("Data Updated Successfully..!")
+    })
+  }
+
+  deleteData(id: any)
+  {
+    // this.afs.collection('categories').doc(id).delete().then(docRef => {
+    //   this.toastr.success("Data Deleted!")
+    // })
+    this.afs.doc(`categories/${id}`).delete().then(docRef => {
+      this.toastr.success("Data Deleted!")
+    })
+  }
+}
